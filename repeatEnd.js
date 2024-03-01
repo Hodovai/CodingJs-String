3d808af7d924649eb58f923c7ea13b216af699ae
Hodovai <eehodovai@gmail.com>
2024-03-01 12:32:47 +0100

    first commit

---
+++ repeatEnd.js
@@ -0,0 +1,13 @@
+// Given a string and an int n, return a string made of n repetitions of the last n characters of the string. You may assume that n is between 0 and the length of the string, inclusive.
+
+// Examples
+
+// repeatEnd('Hello', 3) → llollollo
+// repeatEnd('Hello', 2) → lolo
+// repeatEnd('Hello', 1) → o
+function repeatEnd(str, n){
+    return str.slice(-n).repeat(n)
+  }
+  console.log(repeatEnd("Hello", 3)); // "llollollo"
+  console.log(repeatEnd("Hello", 2)); // "lolo"
+  console.log(repeatEnd("Hello", 1)); // "o"
